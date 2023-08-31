d40ec9b371949836aa110f269fee6c8b7c460bca
JonathanWang3 <jtwang@stu.naperville203.org>
2023-08-31 08:24:42 -0500

    idek whats going on anymore bruh

--- euclid.java
+++ euclid.java
@@ -11,6 +11,7 @@ public class euclid
 {
     public static void main(String[] args){
         int val1, val2, placeholder, remainder, vp1, vp2;
+        boolean bugFix = true;
         Scanner keyboard = new Scanner(System.in);
         
         System.out.println("Enter a numerator and a denominator: ");
@@ -24,15 +25,23 @@ public class euclid
             val1 = val2;
             val2 = placeholder;
         }
-        while (val1 % val2 != 0){
+        if (val2 == 1 || val1 == 1){
+            System.out.println(val1 + " / " + val2);
+            bugFix = false;
+        } else{
+            bugFix = true;
+        }
+        while (bugFix){//#val1 % val2 != 0){
             remainder = val1 % val2;
             while (val2 > remainder){
                 val2 = val2 - remainder;
             }
             val1 = remainder;
         }
-        System.out.println((vp1/val1) + " / " + (vp2/val1));
-        //# hello?
+        if (bugFix){
+            System.out.println((vp1/val1) + " / " + (vp2/val1));
+        }
+            //# hello?
         //# bro
     }    
 }
